f014795e4f9c82a644ff00423c35c91bc8fa414b
kdc7140 <kdc7140@naver.com>
2024-04-07 02:43:22 +0900

    달력, 메인화면 루트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import DiaryMain from "./components/DiaryMain";
 import MusicSearch from "./components/MusicSearch";
-
+import CalendarMain from "./components/CalendarMain";
+import Header from "./components/SpotifyLogin.jsx";
 
 
 function App() {
   return (
     <>
+        <div>
+            <Header/>
+            <div className='neonLine'></div>
+        </div>
       <Routes>
-        <Route path="/" element={<DiaryMain/>} />
+        <Route path="/" element={<CalendarMain/>}  />
+        <Route path="/diary" element={<DiaryMain/>} />
         <Route path="/search" element={<MusicSearch/>} />
+        {/* <Route path="/calendar" element={<CalendarMain/>} /> */}
       </Routes>
     </>
   );
